611f78bcfae7600d293f0e86dde56e6de947f17e
paulanny <paulanimashaun20@gmail.com>
2022-06-15 22:58:40 +0100

    push

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ form.addEventListener('submit', (e)=> {
     if(FirstName.value.trim() === '') {
         errorMsg[0].innerHTML = "First Name cannot be Empty";
         Icons[0].style.opacity ="1";
+        FirstName.style.borderColor = 'red'
     } 
     else{
         errorMsg[0].innerHTML = '';
         Icons[0].style.opacity = '0';
+        FirstName.style.borderColor = 'hsl(246, 25%, 77%)'
     }
 
     if(LastName.value.trim() === '') {
         errorMsg[1].innerHTML = "Last Name cannot be Empty";
         Icons[1].style.opacity ="1"
+        LastName.style.borderColor = 'red'
     }
     //if(!LastName.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)){
         //errorMsg[1].innerHTML = "wrong input";
@@ -31,26 +34,32 @@ form.addEventListener('submit', (e)=> {
     else{
         errorMsg[1].innerHTML = '';
         Icons[1].style.opacity = '0';
+        LastName.style.borderColor = 'hsl(246, 25%, 77%)'
     }
 
 
-    if(LastName.value.trim() === '') {
+    if(password.value.trim() === '') {
         errorMsg[3].innerHTML = "Password cannot be Empty";
         Icons[3].style.opacity ="1"
+        password.style.borderColor = 'red'
     }
     else{
         errorMsg[3].innerHTML = '';
         Icons[3].style.opacity = '0';
+        password.style.borderColor = 'hsl(246, 25%, 77%)'
+
     }
 
     if(email.value.trim() === '') {
         errorMsg[2].innerHTML = "Email cannot be Empty";
        Icons[2].style.opacity ="1"
+       email.style.borderColor = 'red'
         return false;
     }
    else{
        errorMsg[3].innerHTML = '';
        Icons[3].style.opacity = '0';
+       email.style.borderColor = 'hsl(246, 25%, 77%)'
     }
 
     if(!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z][\.][a-z]{2;4}$/)) {
