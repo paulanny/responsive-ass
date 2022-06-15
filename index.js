0efbf1fc567db9057fb355398e36aa1460621b68
paulanny <paulanimashaun20@gmail.com>
2022-06-15 22:18:34 +0100

    push

--- index.js
+++ index.js
@@ -43,15 +43,15 @@ form.addEventListener('submit', (e)=> {
         Icons[3].style.opacity = '0';
     }
 
-    if(email.value.trim() === '') {
-        errorMsg[2].innerHTML = "Email cannot be Empty";
-        Icons[2].style.opacity ="1"
-        return false;
-    }
-    else{
-        errorMsg[3].innerHTML = '';
-        Icons[3].style.opacity = '0';
-    }
+    //if(email.value.trim() === '') {
+      //  errorMsg[2].innerHTML = "Email cannot be Empty";
+       // Icons[2].style.opacity ="1"
+        //return false;
+    //}
+   // else{
+       // errorMsg[3].innerHTML = '';
+       // Icons[3].style.opacity = '0';
+   // }
 
     if(!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z][\.][a-z]{2;4}$/)) {
         errorMsg[2].innerHTML = "Looks like this is not an email";
